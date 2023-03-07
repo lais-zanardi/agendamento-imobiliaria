934dab4b92b487cb5b597f4662bd8bff02e48a13
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-03-07 00:03:57 -0300

    initial commit

---
+++ src/main/java/ada/agendamento/AgendamentoImobiliariaApplication.java
@@ -0,0 +1,13 @@
+package ada.agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoImobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoImobiliariaApplication.class, args);
+	}
+
+}
